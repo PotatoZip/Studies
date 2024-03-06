039592fcd790f46c7e6cb6e664ccc46a5c7d48bb
Wiktor <wiktor.zmiendak@gmail.com>
2024-03-06 08:36:21 +0100

    Modify Lab_1 MO

--- Semestr_IV/Metody_Obliczeniowe/Lab_1.cpp
+++ Semestr_IV/Metody_Obliczeniowe/Lab_1.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
-
 int main() {
-    float f = 1.0f;
-    double d = 1.0;
+    float epsilonFloat = 1.0f;
+    double epsilonDouble = 1.0;
 
-    while ((f + 1.0f) != 1.0f) {
-        f /= 2.0f;
+    int mantysaFloat = 0;
+    int mantysaDouble = 0;
+
+    while ((epsilonFloat + 1.0f) > 1.0f) {
+        epsilonFloat /= 2.0f;
+        mantysaFloat++;
     }
+    epsilonFloat *= 2.0f;
 
-    while ((d + 1.0) != 1.0) {
-        d /= 2.0;
+    while ((epsilonDouble + 1.0) > 1.0) {
+        epsilonDouble /= 2.0;
+        mantysaDouble++;
     }
+    epsilonDouble *= 2.0;
 
-    float EpsilonFloat = f;
-    double EpsilonDouble = d;
-
-    std::cout << "Epsilon float: " << f << "\nEpsilon double: " << d << std::endl;
-
+    std::cout << "Epsilon float: " << epsilonFloat << "\nEpsilon double: " << epsilonDouble << std::endl;
+    std::cout << "Mantysa dla float: " << mantysaFloat - 1 << "\nMantysa dla double: " << mantysaDouble - 1 << std::endl;
     return 0;
 }
