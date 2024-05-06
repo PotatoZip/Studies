3a01c17173dd04493edd575b58dec836c3e98972
Wiktor <wiktor.zmiendak@gmail.com>
2024-05-06 08:49:58 +0200

    Almost done lab 7,8 MO

--- Semestr_IV/Metody_Obliczeniowe/Lab_7/lab_7.cpp
+++ Semestr_IV/Metody_Obliczeniowe/Lab_7/lab_7.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <vector>
+#include <iomanip>
+
+#define N 4
 
 std::vector<std::vector<double>> matrixA = {
     {100.0, -1.0, 2.0, -3.0},
@@ -10,28 +13,161 @@ std::vector<std::vector<double>> matrixA = {
 }; 
 
 std::vector<double> vectorB = {116.0, -226.0, 912.0, -1174.0};
-std::vector<double> vecotrX = {2.0, 2.0, 2.0, 2.0};
+std::vector<double> vectorX = {2.0, 2.0, 2.0, 2.0};
 
 void jacobie();
 void gaussSeidel();
 void sor();
+double residuum(std::vector<double> vector1, std::vector<double> vector2);
+double estymator(std::vector<double> vector1, std::vector<double> vector2);
+void printVector(std::vector<double> vector);
+void printMatrix(std::vector<std::vector<double>> matrix);
 
 int main() {
     jacobie();
+    for (int i = 0; i < N; i++) { vectorX[i] = 2.0; }
     gaussSeidel();
+    for (int i = 0; i < N; i++) { vectorX[i] = 2.0; }
     sor();
 
     return 0;
 }
 
 void jacobie() {
-
+    double tolX = 1e-12;
+    double tolF = 1e-12;
+    int iter = 15;
+    std::vector<double> vectorU = {0.0, 0.0, 0.0, 0.0};
+    std::cout << "-----------------------\n              Metoda Jacobiego\n-----------------------\n" << std::endl;
+    std::cout << std::setw(10) << "Iteracja:";
+    for (int i = 0; i < N; i++) {
+        std::cout << std::setw(10) << "X" << i + 1;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < N; i++) {
+        double D = 1 / matrixA[i][i];
+        vectorB[i] = D * vectorB[i];
+        for (int j = 0; j < N; j++) {
+            matrixA[i][j] = D * matrixA[i][j];
+        }
+    }
+    for (int i = 0; i < iter; i++) {
+        for (int j = 0; j < N; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                if (k != j) {
+                    sum += matrixA[j][k] * vectorX[k];
+                }
+            }
+            vectorU[j] = vectorB[j] - sum;
+        }
+        std::cout << std::setw(10) << "Iteracja: " << i;
+        for (int j = 0; j < N; j++) {
+            std::cout << std::setw(10) << vectorX[j];
+        }
+        std::cout << std::setw(20) << "Estymator: " << estymator(vectorU, vectorX);
+        std::cout << std::setw(20) << "Residuum: " << residuum(vectorU, vectorB) << std::endl;
+        if (estymator(vectorU, vectorX) < tolX || residuum(vectorU, vectorB) < tolF) { break; }
+        for (int j = 0; j < N; j++) { vectorX[j] = vectorU[j];}
+    }
 }
 
 void gaussSeidel() {
-
+    double tolX = 1e-12;
+    double tolF = 1e-12;
+    int iter = 15;
+    std::vector<double> vectorU = {0.0, 0.0, 0.0, 0.0};
+    std::cout << "-----------------------\n              Metoda Gaussa Seidela\n-----------------------\n" << std::endl;
+    std::cout << std::setw(10) << "Iteracja:";
+    for (int i = 0; i < N; i++) {
+        std::cout << std::setw(10) << "X" << i + 1;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < iter; i++) {
+        for (int j = 0; j < N; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                if (k != j) {
+                    sum += matrixA[j][k] * vectorX[k];
+                }
+            }
+            vectorX[j] = (vectorB[j] - sum) / matrixA[j][j];
+        }
+        std::cout << std::setw(10) << "Iteracja: " << i;
+        for (int j = 0; j < N; j++) {
+            std::cout << std::setw(10) << vectorX[j];
+        }
+        std::cout << std::setw(20) << "Estymator: " << estymator(vectorU, vectorX);
+        std::cout << std::setw(20) << "Residuum: " << residuum(vectorX, vectorB) << std::endl;
+        if (estymator(vectorX, vectorU) < tolX || residuum(vectorX, vectorB) < tolF) { break; }
+    }
 }
 
 void sor() {
     double w = 1.0/2.0;
+    double tolX = 1e-12;
+    double tolF = 1e-12;
+    int iter = 30;
+    std::vector<double> vectorU = {0.0, 0.0, 0.0, 0.0};
+    std::cout << "-----------------------\n              Metoda SOR\n-----------------------\n" << std::endl;
+    std::cout << std::setw(10) << "Iteracja:";
+    for (int i = 0; i < N; i++) {
+        std::cout << std::setw(10) << "X" << i + 1;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < iter; i++) {
+        for (int j = 0; j < N; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                if (k != j) {
+                    sum += matrixA[j][k] * vectorX[k];
+                }
+            }
+            vectorX[j] = (1 - w) * vectorX[j] + w * (vectorB[j] - sum) / matrixA[j][j];
+        }
+        std::cout << std::setw(10) << "Iteracja: " << i;
+        for (int j = 0; j < N; j++) {
+            std::cout << std::setw(10) << vectorX[j];
+        }
+        std::cout << std::setw(20) << "Estymator: " << estymator(vectorU, vectorX);
+        std::cout << std::setw(20) << "Residuum: " << residuum(vectorX, vectorB) << std::endl;
+        if (estymator(vectorX, vectorU) < tolX || residuum(vectorX, vectorB) < tolF) { break; }
+    }
 }
+
+double residuum(std::vector<double> vector1, std::vector<double> vector2) {
+    double residuumMax = 0.0;
+    for (int i = 0; i < N; i++) {
+        double residuum = 0.0;
+        for (int j = 0; j < N; j++) {
+            residuum += matrixA[i][j] * vector1[j];
+        }
+        residuum -= vector2[i];
+        residuumMax = std::max(fabs(residuum), residuumMax);
+    }
+    return residuumMax;
+}
+
+double estymator(std::vector<double> vector1, std::vector<double> vector2) {
+    double estymator = 0.0;
+    for (int i = 0; i < N; i++) {
+        estymator = std::max(fabs(vector1[i] - vector2[i]), estymator);
+    }
+    return estymator;
+}
+
+void printVector(std::vector<double> vector) {
+    for (int i = 0; i < N; i++) {
+        std::cout << std::setw(9) << vector[i] << " " << std::endl;
+    }
+}
+
+void printMatrix(std::vector<std::vector<double>> matrix) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << matrix[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
--- Semestr_IV/Metody_Obliczeniowe/Lab_8/lab_8.cpp
+++ Semestr_IV/Metody_Obliczeniowe/Lab_8/lab_8.cpp
@@ -1,9 +1,86 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
+#define _USE_MATH_DEFINES
+
+template <typename T>
+T forwardDifference(T x, T h) { return (sin(x + h) - sin(x)) / h; }
+template <typename T>
+T backwardDifference(T x, T h) { return (sin(x) - sin(x - h)) / h; }
+template <typename T>
+T centralDifference(T x, T h) { return (sin(x + h) - sin(x - h)) / (static_cast<T>(2)*h); }
+template <typename T>
+T forwardDifference3Points(T x, T h) { return (-static_cast<T>(3.0) / static_cast<T>(2.0) * sin(x) + static_cast<T>(2.0) * sin(x + h) - static_cast<T>(1.0) / static_cast<T>(2.0) * sin(x + h + h)) / h; }
+template <typename T>
+T backwardDifference3Points(T x, T h) { return ((static_cast<T>(1.0) / static_cast<T>(2.0) * sin(x - h - h)) - (static_cast<T>(2.0) * sin(x - h)) + (static_cast<T>(3.0) / static_cast<T>(2.0) * sin(x))) / h; }
+template <typename T>
+void calculate(std::string fileName, bool typ) {
+    T startPoint = static_cast<T>(0.0);
+    T centerPoint = static_cast<T>(M_PI / static_cast<T>(4.0));
+    T endPoint = static_cast<T>(M_PI / static_cast<T>(2.0));
+    T h = static_cast<T>(0.1);
+
+    std::vector<T> solutions;
+    std::vector<T> vectorP1;
+    std::vector<T> vectorP2;
+
+    for (int i = 0; i < 180; i++) {
+        if (h < 10e-15 && !typ) { break; }
+        if (h < 10e-7 && typ) { break; }
+        if (i == 0) {
+            vectorP1.push_back(log10(h));
+            vectorP1.push_back(log10(abs(forwardDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP1.push_back(log10(abs(backwardDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP1.push_back(log10(abs(centralDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP1.push_back(log10(abs(forwardDifference(startPoint, h) - cos(startPoint))));
+            vectorP1.push_back(log10(abs(forwardDifference(endPoint, h) - cos(endPoint))));
+            vectorP1.push_back(log10(abs(forwardDifference3Points(startPoint, h) - cos(startPoint))));
+            vectorP1.push_back(log10(abs(backwardDifference3Points(endPoint, h) - cos(endPoint))));
+        }
+        if (i == 6) {
+            vectorP2.push_back(log10(h));
+            vectorP2.push_back(log10(abs(forwardDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP2.push_back(log10(abs(backwardDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP2.push_back(log10(abs(centralDifference(centerPoint, h) - cos(centerPoint))));
+            vectorP2.push_back(log10(abs(forwardDifference(startPoint, h) - cos(startPoint))));
+            vectorP2.push_back(log10(abs(forwardDifference(endPoint, h) - cos(endPoint))));
+            vectorP2.push_back(log10(abs(forwardDifference3Points(startPoint, h) - cos(startPoint))));
+            vectorP2.push_back(log10(abs(backwardDifference3Points(endPoint, h) - cos(endPoint))));
+        }
+
+        solutions.push_back(log10(h));
+        solutions.push_back(log10(abs(forwardDifference(centerPoint, h) - cos(centerPoint))));
+        solutions.push_back(log10(abs(backwardDifference(centerPoint, h) - cos(centerPoint))));
+        solutions.push_back(log10(abs(centralDifference(centerPoint, h) - cos(centerPoint))));
+        solutions.push_back(log10(abs(forwardDifference(startPoint, h) - cos(startPoint))));
+        solutions.push_back(log10(abs(forwardDifference(endPoint, h) - cos(endPoint))));
+        solutions.push_back(log10(abs(forwardDifference3Points(startPoint, h) - cos(startPoint))));
+        solutions.push_back(log10(abs(backwardDifference3Points(endPoint, h) - cos(endPoint))));
+
+        h = h / static_cast<T>(1.2);
+    }
+
+    std::fstream file1, file2;
+        file1.open(fileName+".txt", std::ios::out);
+        file2.open(fileName+"_precision"+".txt", std::ios::out);
+        for (int i = 0; i < solutions.size(); i++) {
+            file1 << std::scientific << solutions[i] << std::endl;
+        }
+        for (int i = 0; i < vectorP1.size(); i++) {
+            T a = (vectorP2[i] - vectorP1[i]) / (vectorP2[0] - vectorP1[0]);
+            file2 << a << std::endl;
+        }
+        file1.close();
+        file2.close();
+}
 
 
 int main() {
-
-    
+    calculate<float>("floatSolution", 1);
+    calculate<double>("doubleSolution", 0);
 
     return 0;
 }
