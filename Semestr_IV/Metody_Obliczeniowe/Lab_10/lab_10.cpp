67df5d352c435566ce320d9c0948587665210efb
Wiktor <wiktor.zmiendak@gmail.com>
2024-05-15 11:31:53 +0200

    Do lab9,10 MO

--- Semestr_IV/Metody_Obliczeniowe/Lab_10/lab_10.cpp
+++ Semestr_IV/Metody_Obliczeniowe/Lab_10/lab_10.cpp
@@ -3,7 +3,104 @@
 #include <iomanip>
 #include <fstream>
 
-int main() {
+int dl_ = 15;
 
-    return 0;
+double analiticSolution(double t) { return 1 - exp(-10.0 * (t + atan(t))); }
+
+double bme(double step, double tmax) {
+    double y = 0.0;
+    for (double t = 0.0; t < tmax; t += step) { y = y - step * (y - 1.0) * (10.0 * t * t + 20.0) / (t * t + 1.0); }
+    return y;
+}
+
+double pme(double step, double tmax) {
+    double y = 0.0;
+    double fraction;
+    for (double t = 0.0; t < tmax; t += step) {
+        fraction = (10.0 * (t + step) * (t + step) + 20.0) / ((t + step) * (t + step) + 1.0);
+        y = (y + step * fraction) / (1 + step * fraction);
+    }
+    return y;
+}
+
+double mt(double step, double tmax) {
+    double y = 0.0, u1, u2;
+    for (double t = 0.0; t < tmax; t += step) {
+        u1 = ((10.0 * t * t + 20.0) / (t * t + 1.0));
+        u2 = (10.0 * (t + step) * (t + step) + 20.0) / ((t + step) * (t + step) + 1.0);
+        y = ((-step / 2.0) * (u1 * (y - 1.0) - u2) + y) / (1.0 + (step / 2.0) * u2);
+    }
+    return y;
+}
+
+double mtError(double step, int N) {
+    double maxBlad = 0.0, t = 0, y = 0.0;
+    double exactValue, u1, u2;
+    for (int i = 0; i < N; i++) {
+        u1 = ((10.0 * t * t + 20.0) / (t * t + 1.0));
+        u2 = (10.0 * (t + step) * (t + step) + 20.0) / ((t + step) * (t + step) + 1.0);
+        y = ((-step / 2.0) * (u1 * (y - 1.0) - u2) + y) / (1.0 + (step / 2.0) * u1);
+        exactValue = analiticSolution(t);
+        if (fabs(exactValue - y) > maxBlad)
+            maxBlad = fabs(exactValue);
+        t += step;
+    }
+    return maxBlad;
+}
+
+double bmeError(double step, int n) {
+    double maxBlad = 0.0, t = 0, y = 0.0, exactValue;
+    for (int i = 0; i < n; i++) {
+        y = y - step * (y - 1.0) * ((10.0 * t * t + 20.0) / (t * t + 1.0));
+        exactValue = analiticSolution(t);
+        if (fabs(exactValue - y) > maxBlad)
+            maxBlad = fabs(exactValue - y);
+        t += step;
+    }
+    return maxBlad;
+}
+
+
+double pmeError(double step, int n) {
+    double maxBlad = 0.0, y = 0.0, t = 0, exactValue, fraction;
+    for (int i = 0; i < n; i++) {
+        fraction = (10.0 * (t + step) * (t + step) + 20.0) / ((t + step) * (t + step) + 1.0);
+        y = (y + step * fraction) / (1 + step * fraction);
+        exactValue = analiticSolution(t);
+        if (fabs(exactValue - y) > maxBlad)
+            maxBlad = fabs(exactValue - y);
+        t += step;
+    }
+    return maxBlad;
+}
+
+int main() {
+    double step = 0.1, euler_b, euler_p, trapezow, rozwAnalityczne;
+    int tmax = 100;
+    std::fstream out_b, out_s, out_ns;
+    out_b.open("errors.txt", std::fstream::out);
+    for (step; step > 1e-18; step *= .9) {
+        euler_b = log10(bmeError(step, tmax));
+        euler_p = log10(pmeError(step, tmax));
+        trapezow = log10(mtError(step, tmax));
+        out_b << std::setw(dl_) << log10(step) << std::setw(dl_) << euler_b << std::setw(dl_) << euler_p << std::setw(dl_) << trapezow << std::endl;
+    }
+    out_b.close();
+    step = 0.01;
+    out_s.open("stable.txt", std::fstream::out);
+    for (double t = 0; t < 1; t += 0.01) {
+        rozwAnalityczne = analiticSolution(t);
+        euler_b = bme(step, t);
+        euler_p = pme(step, t);
+        trapezow = mt(step, t);
+        out_s << std::setw(dl_) << t << std::setw(dl_) << rozwAnalityczne << std::setw(dl_) << euler_b << std::setw(dl_) << euler_p << std::setw(dl_) << trapezow << std::endl;
+    }
+    out_s.close();
+    out_ns.open("unstable.txt", std::fstream::out);
+    for (double t = 0; t < 1; t += 0.01) {
+        rozwAnalityczne = analiticSolution(t);
+        euler_b = bme(0.2, t);
+        out_ns << std::setw(dl_) << t << std::setw(dl_) << rozwAnalityczne << std::setw(dl_) << euler_b << std::endl;
+    }
+    out_ns.close();
 }
--- Semestr_IV/Metody_Obliczeniowe/Lab_9/lab_9.cpp
+++ Semestr_IV/Metody_Obliczeniowe/Lab_9/lab_9.cpp
@@ -2,8 +2,152 @@
 #include <cmath>
 #include <iomanip>
 #include <fstream>
+#include <algorithm>
+#include <sstream>
+
+const double p = 1.0;
+const double q = 0.0;
+const double r = -4.0;
+const double alfa = 0.0;
+const double bet = 1.0;
+const double gamma = -1.0;
+const double fi = 0.0;
+const double psi = 1.0;
+const double teta = 0.0;
+const double start = 0.0;
+const double stop = 1.0;
+
+template <typename T>
+T *alocation(const int size) {
+    T *vector = new T[size];
+    return vector;
+}
+
+template <typename T>
+void deletion(T *vector) { delete[] vector; }
+
+double solution(double x) {
+    return (exp(2.0 - 2.0 * x) - 4.0 * exp(4.0 - x * 2.0) + 4.0 * exp(x * 2.0) - exp(2.0 + 2.0 * x) - x + x * exp(4.0)) / (4.0 - 4.0 * exp(4.0));
+}
+
+void thomas(const double *l, const double *d, const double *u, const double *b, double *x, int N) {
+    auto *bCopy = alocation<double>(N);
+    auto *dCopy = alocation<double>(N);
+    bCopy[0] = b[0];
+    dCopy[0] = d[0];
+    for (int i = 1; i < N; i++) { dCopy[i] = d[i] - l[i - 1] * (u[i - 1] / dCopy[i - 1]); }
+    for (int i = 1; i < N; i++) { bCopy[i] = b[i] - l[i - 1] * bCopy[i - 1] / dCopy[i - 1]; }
+    x[N - 1] = bCopy[N - 1] / dCopy[N - 1];
+    for (int i = N - 2; i >= 0; i--) { x[i] = (bCopy[i] - u[i] * x[i + 1]) / dCopy[i]; }
+    deletion<double>(dCopy);
+    deletion<double>(bCopy);
+}
+
+double threePointDys(double h, int N) {
+    std::fstream threePoint;
+    threePoint.open("three_point.txt", std::ios_base::app);
+    threePoint << std::scientific;
+    std::cout.precision(10);
+    double *l = alocation<double>(N),
+    *d = alocation<double>(N),
+    *u = alocation<double>(N),
+    *b = alocation<double>(N),
+    *x = alocation<double>(N),
+    *error = alocation<double>(N),
+    xp1 = start, xp2 = start;
+    u[0] = alfa / h;
+    d[0] = bet - alfa / h;
+    b[0] = -gamma;
+    for (int i = 1; i < N - 1; i++) {
+        l[i - 1] = p / (h * h) - q / (2.0 * h);
+        d[i] = (-2.0 * p) / (h * h) + r;
+        u[i] = p / (h * h) + q / (2.0 * h);
+        b[i] = (xp1 + i * h);
+    }
+    l[N - 2] = -fi / h;
+    d[N - 1] = -fi / h + psi;
+    b[N - 1] = -teta;
+    thomas(l, d, u, b, x, N);
+    for (int i = 0; i < N; i++) {
+        error[i] = fabs(x[i] - solution(xp2));
+        xp2 += h;
+    }
+    if (N == 202) {
+        for (int i = 0; i < N; i++) {
+            threePoint << xp1 << "\t" << x[i] << std::endl;
+            xp1 += h;
+        }
+    }
+    int index = std::max_element(error, error + N) - error;
+    deletion<double>(l);
+    deletion<double>(d);
+    deletion<double>(u);
+    deletion<double>(x);
+    deletion<double>(b);
+    return error[index];
+}
+
+double numerowDys(double h, int N) {
+    std::fstream numerow, analiticSolution;
+    numerow.open("numerow.txt", std::ios_base::app);
+    analiticSolution.open("analitic_solution.txt", std::ios_base::app);
+    analiticSolution << std::scientific;
+    numerow << std::scientific;
+    double *l = alocation<double>(N),
+    *d = alocation<double>(N),
+    *u = alocation<double>(N),
+    *b = alocation<double>(N),
+    *x = alocation<double>(N),
+    *error = alocation<double>(N),
+    xp1 = start, xp2 = start;
+    u[0] = alfa / h;
+    d[0] = bet - alfa / h;
+    b[0] = -gamma;
+    for (int i = 1; i < N - 1; i++) {
+        l[i - 1] = p / (h * h) + r / 12.0;
+        d[i] = (-2.0 * p) / (h * h) + r * (10.0 / 12.0);
+        u[i] = p / (h * h) + r / 12.0;
+        b[i] = (xp1 + i * h - h) / 12.0 + (10.0 / 12.0) * (xp1 + i * h) + (xp1 + i * h + h) / 12.0;
+    }
+    l[N - 2] = -fi / h;
+    d[N - 1] = -fi / h + psi;
+    b[N - 1] = -teta;
+    thomas(l, d, u, b, x, N);
+    for (int i = 0; i < N; i++) {
+        error[i] = fabs(x[i] - solution(xp2));
+        xp2 += h;
+    }
+    if (N == 202) {
+        for (int i = 0; i < N; i++) {
+            numerow << xp1 << "\t" << x[i] << "\t" << std::endl;
+            analiticSolution << xp1 << "\t" << solution(xp1) << std::endl;
+            xp1 += h;
+        }
+    }
+    int index = std::max_element(error, error + N) - error;
+    deletion<double>(l);
+    deletion<double>(d);
+    deletion<double>(u);
+    deletion<double>(x);
+    deletion<double>(b);
+    analiticSolution.close();
+    numerow.close();
+    return error[index];
+}
 
 int main() {
+    double h, numerowError, threePointError;
+    std::fstream errors;
+    errors.open("errors.txt", std::fstream::out);
+    errors << std::scientific;
+    std::cout.precision(10);
+    for (int i = 2; i < 100000; i += 100) {
+        h = (stop - start) / (i - 1);
+        threePointError = (threePointDys(h, i));
+        numerowError = (numerowDys(h, i));
+        errors << log10(h) << "\t" << log10(threePointError) << "\t" << log10(numerowError) << std::endl;
+    }
+    errors.close();
 
     return 0;
 }
